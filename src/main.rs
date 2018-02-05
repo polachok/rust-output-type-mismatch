bbbd780245731dd1167db211d62b93909e11b9ae
Alexander Polyakov <aleksandr.polyakov@kupibilet.ru>
2018-02-05 16:48:07 +0300

    Minify

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,61 @@
-extern crate capnp;
-mod consumer;
+pub trait Reader<'a>: Sized {
+    fn get(bytes: &'a [u8]) -> Self;
+}
 
-use consumer::*;
+pub trait Owned<'a> {
+    type Reader: Reader<'a>;
+}
+
+pub trait ConsumerService {
+    type Item: for<'x> Owned<'x>;
+
+    fn call(_item: <Self::Item as Owned>::Reader) {
+        unimplemented!()
+    }
+}
+
+struct M;
+struct MReader<'a>(&'a [u8]);
+
+impl<'a> Owned<'a> for M {
+    type Reader = MReader<'a>;
+}
+
+impl<'a> Reader<'a> for MReader<'a> {
+    fn get(bytes: &'a [u8]) -> Self {
+        MReader(bytes)
+    }
+}
 
 struct My;
 
 impl ConsumerService for My {
-	type Item = capnp::any_pointer::Owned;
+	type Item = M;
 }
 
-impl BatchConsumerService for My {
-	fn call_batch(&mut self, items: Vec<capnp::any_pointer::Reader>) {
-        for item in items {
-            //println!("{:?}", item);
-        }
-	}
+fn consume_loop<T, M, P>(
+    process: P,
+) where
+    T: ConsumerService + 'static,
+    M: for<'x> Owned<'x>,
+    P: Fn(Vec<<M as Owned>::Reader>) -> bool + Sync + Send + 'static,
+{
+
+    let readers: Vec<M::Reader> = unsafe { std::mem::uninitialized() };
+    process(readers);
+}
+
+pub fn t<S: ConsumerService + 'static>(s: S) {
+     consume_loop::<S, S::Item, _>(
+        move |batch| {
+            for item in batch {
+                S::call(item);
+            }
+            false
+        },
+    )
 }
 
 fn main() {
-    Consumer::new().batch(|| My);
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    t(My);
 }
